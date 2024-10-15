840cdc78f7379c7099c5d2769658e873d2b9a524
Jeremi Lewandowski <jeremilew@gmail.com>
2024-10-15 22:32:23 +0200

    innit

---
+++ image_classification_yolo/model_training.py
@@ -0,0 +1,16 @@
+﻿from ultralytics import YOLO
+
+def train_model():
+    model = YOLO('yolo11n-cls.pt')
+
+    model.train(
+        data='C:\\Users\\jerem\\Desktop\\classification data\\images',
+        epochs=2,
+        batch=64,
+        imgsz=640,
+        device=0,
+        patience=5
+    )
+
+if __name__ == '__main__':
+    train_model()
