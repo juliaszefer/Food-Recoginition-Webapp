4527de1d5aa5eac5edc5211874bc24b2c3bb8ccd
Jeremi Lewandowski <102260483+iMeReJ22@users.noreply.github.com>
2024-11-13 18:43:57 +0100

    dodano metodę do zwracania dictionary z odpowiednimi wynikami
    zmina budowy pliku predict, i dodanie metody predict, predict_from_path

--- image_classification_yolo/main.py
+++ image_classification_yolo/main.py
@@ -1,1 +1,10 @@
-﻿
+﻿from image_classification_yolo.predict import Predict
+from PIL import Image
+
+prediction = Predict()
+
+x = input("Input image number: ")
+# predict(f'./test_images/{x}.jpg')
+img = Image.open(f'./test_images/{x}.jpg')
+res = prediction.predict(img)
+Predict.print_results(res)
